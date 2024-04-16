2b514cb508a10c78ddf83b6502b3d7d35cc948dd
paulbg <paulbg@hey.com>
2024-04-16 08:25:04 +0300

    Implement a constant for credit score threshold

--- src/main/java/ee/taltech/inbankbackend/config/DecisionEngineConstants.java
+++ src/main/java/ee/taltech/inbankbackend/config/DecisionEngineConstants.java
@@ -11,4 +11,5 @@ public class DecisionEngineConstants {
     public static final Integer SEGMENT_1_CREDIT_MODIFIER = 100;
     public static final Integer SEGMENT_2_CREDIT_MODIFIER = 300;
     public static final Integer SEGMENT_3_CREDIT_MODIFIER = 1000;
+    public static final float CREDIT_SCORE_THRESHOLD = 1.0f;
 }
